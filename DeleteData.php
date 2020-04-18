bb1e587a90dfef3c6be239700a9ae39c3f41bc66
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-19 03:49:22 +0700

    Update DeleteData.php

--- DeleteData.php
+++ DeleteData.php
@@ -1,7 +1,71 @@
 <!DOCTYPE html>
 <html>
-<body>
+<title>HOME PAGE</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<link rel="stylesheet" href="image/w3.css">
+<style>
+* {box-sizing: border-box}
+img {vertical-align: middle;}
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+.navbar {
+  width: 100%;
+  background-color: #555;
+  overflow: auto;
+}
+.navbar a {
+  float: left;
+  padding: 12px;
+  color: white;
+  text-decoration: none;
+  font-size: 17px;
+  width: 25%; /* Four links of equal widths */
+  text-align: center;
+}
+.navbar a:hover {
+  background-color: #000;
+}
+.navbar a.active {
+  background-color: #4CAF50;
+}
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+    width: 100%;
+    text-align: left;
+  }
+}
+</style>
+<head>
+</head><body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+    <a href="index.html" class="w3-bar-item w3-button">ATN shop</a>
+    <!-- Right-sided navbar links. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="ConnectToDB.php" class="w3-bar-item w3-button">View</a>
+      <a href="InsertData.php" class="w3-bar-item w3-button">Insert</a>
+      <a href="UpdateData.php" class="w3-bar-item w3-button">Update</a>
+      <a href="DeleteData.php" class="w3-bar-item w3-button">Delete</a>
+    </div>
+  </div>
+</div>
+</head>
+<body>
+  <h1>INSERT DATA TO DATABASE</h1>
+</body>
 <h1>INSERT DATA TO DATABASE</h1>
 
 <?php
@@ -42,5 +106,5 @@ if(is_null ($_POST[toyid])== FALSE)  {
         echo "Error updating record. ";
     }}
 ?>
-</body>
+
 </html>
