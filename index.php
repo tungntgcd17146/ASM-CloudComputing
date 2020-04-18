b6bea3ac8fa72b53ea1b824d0e98baad52296399
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-19 04:14:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ img {vertical-align: middle;}
 
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
-  <img class="w3-image" src="image/toys.jpg" alt="Toys" width="1600" height="800">
+  <img class="w3-image" src="bus.jpg" alt="Toys" width="1600" height="800">
   <div class="w3-display-bottomleft w3-padding-large w3-opacity">
     <h1 class="w3-xxlarge">ATN toys shop</h1>
   </div>
