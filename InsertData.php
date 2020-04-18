8190b378854fa2fbfe5374b3bd32d69609dce6f5
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-19 02:16:15 +0700

    Update InsertData.php

--- InsertData.php
+++ InsertData.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
 <title>Insert data to PostgreSQL</title>
@@ -14,8 +14,8 @@ list-style: none;
 <h2>Enter data into table</h2>
 <ul>
     <form name="InsertData" action="InsertData.php" method="POST" >
-<li>ID:</li><li><input type="text" name="id" /></li>
-<li>Name:</li><li><input type="text" name="name" /></li>
+<li>toyID:</li><li><input type="text" name="toyid" /></li>
+<li>Toy Name:</li><li><input type="text" name="toyname" /></li>
 
 <li><input type="submit" /></li>
 </form>
@@ -25,13 +25,13 @@ list-style: none;
 
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=127.0.0.1:49797;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
 }  else {
      
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-18-233-137-77.compute-1.amazonaws.com;port=5432;user=cibttxrjklxbqp;password=be605ce69ac7c5cf26b6b3a833e65b243685409a9d385e0a9872bdd17299b098;
-        dbname=dfuhjimp17l4vf",
+        "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=zyspzjwbrlxfnk;password=95402f2fcd09500f7ad877a328cb24cb0ac00800666b42159462534ac9619b11
+        ;dbname=d7f8iof0djq8lo",
         $db["host"],
         $db["port"],
         $db["user"],
@@ -44,12 +44,12 @@ if($pdo === false){
      echo "ERROR: Could not connect Database";
 }
 
-$sql = "INSERT INTO managerATN(id, name)"
-        . " VALUES('$_POST[id]','$_POST[name]')";
+$sql = "INSERT INTO toy(toyid, toyname)"
+        . " VALUES('$_POST[toyid]','$_POST[toyname]')";
 $stmt = $pdo->prepare($sql);
 //$stmt->execute();
- if (is_null($_POST[id])) {
-   echo "id must be not null";
+ if (is_null($_POST[toyid])) {
+   echo "toyid must be not null";
  }
  else
  {
