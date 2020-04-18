9744e234b185ec147d2df537d358df16e14c3db5
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-19 02:16:57 +0700

    Update UpdateData.php

--- UpdateData.php
+++ UpdateData.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>Update DATA TO DATABASE</h1>
-
-<?php
 
 echo "Update database!";
 ?>
 <ul>
     <form name="UpdateData" action="UpdateData.php" method="POST" >
-<li>ID:</li><li><input type="text" name="id" /></li>
-<li>Name:</li><li><input type="text" name="name" /></li>
+<li>toyID:</li><li><input type="text" name="toyid" /></li>
+<li>Toy Name:</li><li><input type="text" name="toyname" /></li>
 
 <li><input type="submit" /></li>
 </form>
@@ -21,13 +14,13 @@ echo "Update database!";
 
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=127.0.0.1:49797;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
 }  else {
      
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-18-233-137-77.compute-1.amazonaws.com;port=5432;user=cibttxrjklxbqp;password=be605ce69ac7c5cf26b6b3a833e65b243685409a9d385e0a9872bdd17299b098;
-        dbname=dfuhjimp17l4vf",
+        "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=zyspzjwbrlxfnk;password=95402f2fcd09500f7ad877a328cb24cb0ac00800666b42159462534ac9619b11
+        ;dbname=d7f8iof0djq8lo",
         $db["host"],
         $db["port"],
         $db["user"],
@@ -35,9 +28,9 @@ if (empty(getenv("DATABASE_URL"))){
         ltrim($db["path"], "/")
    ));
 }  
-$sql = "UPDATE managerATN SET  name = '$_POST[name]' WHERE id = '$_POST[id]'";
+$sql = "UPDATE toy SET  toyname = '$_POST[toyname]' WHERE toyid = '$_POST[toyid]'";
       $stmt = $pdo->prepare($sql);
-if(is_null ($_POST[id])== FALSE)  {    
+if(is_null ($_POST[toyid])== FALSE)  {    
 if($stmt->execute() == TRUE){
     echo "Record updated successfully.";
 } else {
