8aa899a10ebfe1d7b71d9a4db70221b83148c7ae
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-19 02:08:58 +0700

    Update DeleteData.php

--- DeleteData.php
+++ DeleteData.php
@@ -17,7 +17,7 @@ echo "Insert database!";
 
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=127.0.0.1:49797;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
 }  else {
      
    $db = parse_url(getenv("DATABASE_URL"));
