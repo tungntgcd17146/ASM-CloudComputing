9760449d50a0fcfa4e83b7bb1a6beef254d0ffc5
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-19 02:10:50 +0700

    Update UpdateData.php

--- UpdateData.php
+++ UpdateData.php
@@ -21,7 +21,7 @@ echo "Update database!";
 
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=127.0.0.1:49797;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
 }  else {
      
    $db = parse_url(getenv("DATABASE_URL"));
@@ -35,7 +35,7 @@ if (empty(getenv("DATABASE_URL"))){
         ltrim($db["path"], "/")
    ));
 }  
-$sql = "UPDATE toy SET  name = '$_POST[name]' WHERE id = '$_POST[id]'";
+$sql = "UPDATE managerATN SET  name = '$_POST[name]' WHERE id = '$_POST[id]'";
       $stmt = $pdo->prepare($sql);
 if(is_null ($_POST[id])== FALSE)  {    
 if($stmt->execute() == TRUE){
