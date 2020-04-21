6494c93969de239c7af1ee1b726c165f9118c254
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-21 18:07:00 +0700

    Update InsertData.php

--- InsertData.php
+++ InsertData.php
@@ -81,13 +81,12 @@ img {vertical-align: middle;}
 
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
 }  else {
      
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=zyspzjwbrlxfnk;password=95402f2fcd09500f7ad877a328cb24cb0ac00800666b42159462534ac9619b11
-        ;dbname=d7f8iof0djq8lo",
+        "host=ec2-18-233-137-77.compute-1.amazonaws.com;port=5432;user=cibttxrjklxbqp;password=be605ce69ac7c5cf26b6b3a833e65b243685409a9d385e0a9872bdd17299b098;dbname=dfuhjimp17l4vf",
         $db["host"],
         $db["port"],
         $db["user"],
