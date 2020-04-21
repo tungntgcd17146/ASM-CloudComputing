096ad6e7341efad2e26644c4978909a0c3d2f203
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-21 18:15:10 +0700

    Update ConnectToDB.php

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -77,7 +77,7 @@ echo "Hello, this is ATN toy shop";
 
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=tungntgcd17146', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
 }  else {
      echo '<p>The DB exists</p>';
      echo getenv("dbname");
