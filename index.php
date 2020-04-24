ecb6e3cac1eee8efd30dbb3e084f3430ed59f824
tungntgcd17146 <53217325+tungntgcd17146@users.noreply.github.com>
2020-04-25 01:52:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 <link rel="stylesheet" href="image/w3.css">
 <style>
 * {box-sizing: border-box}
@@ -15,13 +14,11 @@ img {vertical-align: middle;}
 .slideshow-container {
   max-width: 1000px;
   position: relative;
-  margin: auto;
-}
+  margin: auto;}
 .navbar {
   width: 100%;
   background-color: #555;
-  overflow: auto;
-}
+  overflow: auto;}
 .navbar a {
   float: left;
   padding: 12px;
@@ -29,25 +26,20 @@ img {vertical-align: middle;}
   text-decoration: none;
   font-size: 17px;
   width: 25%; /* Four links of equal widths */
-  text-align: center;
-}
+  text-align: center;}
 .navbar a:hover {
-  background-color: #000;
-}
+  background-color: #000;}
 .navbar a.active {
-  background-color: #4CAF50;
-}
+  background-color: #4CAF50;}
 @media screen and (max-width: 500px) {
   .navbar a {
     float: none;
     display: block;
     width: 100%;
     text-align: left;
-  }
-}
+  }}
 </style>
 </head><body>
-
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
@@ -61,13 +53,11 @@ img {vertical-align: middle;}
     </div>
   </div>
 </div>
-
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
   <img class="w3-image" src="bus.jpg" alt="Toys" width="1600" height="800">
   <div class="w3-display-bottomleft w3-padding-large w3-opacity">
     <h1 class="w3-xxlarge">ATN toy company</h1>
   </div>
-</header>
-     
+</header>    
 </html>
